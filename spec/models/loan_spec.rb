840c6715cfcff2463995e846465f37cf572ccf07
peterjacobson <peterjacobson.nz@gmail.com>
2015-08-18 15:36:31 +1200

    tests for loan associations

--- spec/models/loan_spec.rb
+++ spec/models/loan_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Loan, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it {should have_many(:bikes)}
+	it {should have_one(:person)}
+  
 end
