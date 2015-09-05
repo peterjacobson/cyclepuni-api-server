114af8db42391ad8fa0b5a443ba96b9b6dd95f89
younggabrielle <gmyoung321@gmail.com>
2015-09-05 15:19:55 +1200

    add spec for creating a loan

--- spec/features/home_pages_spec.rb
+++ spec/features/home_pages_spec.rb
@@ -5,11 +5,13 @@ require 'devise'
 RSpec.feature "HomePages", type: :feature do
   scenario 'Admin can log in and see dashboard ' do
     sign_in
-    expect(page).to have_content "New loan"
+    expect(page).to have_content "loan"
     expect(page).to have_content "Return bike"
   end
 
   scenario 'Admin can log out' do
-
+    sign_in
+    click_on "Log out"
+    expect(current_path).to eq root_path
   end
 end
--- spec/features/loan_bikes_spec.rb
+++ spec/features/loan_bikes_spec.rb
@@ -2,7 +2,29 @@ require 'rails_helper'
 
 
 RSpec.feature "LoanBikes", type: :feature do
-  scenario 'Create loan from admin page' do
-    visit
+  before do
+
+  end
+
+  scenario 'can create a loan' do
+    sign_in
+
+    click_on "New bike loan"
+    loan = attributes_for(:loan)
+
+    click_on "Add bike"
+    click_on "Borrow"
+
+    click_on "Borrower"
+    click_on "New person"
+    borrower = attributes_for(:person)
+    fill_in "Name", with: borrower.name
+    fill_in "Phone", with: borrower.phone
+    fill_in "Email", with: borrower.email
+    click_on "Add this lovely person"
+
+    fill_in "Contribution notes", with: loan.contribution_notes
+    click_on "Away you go, Happy biking!"
+    expect(Loans.all.size).to eq 1
   end
 end
